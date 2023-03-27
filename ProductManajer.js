bd882dfe32eee4b71d24d12bee76036e5264e262
David Scoffield <davidscoffield01@gmail.com>
2023-03-27 13:20:13 -0300

    1er entregable

---
+++ ProductManajer.js
@@ -0,0 +1,77 @@
+class ProductManager {
+  constructor() {
+    this.products = []
+  }
+
+  getProducts = () => this.products
+
+  getProductsById = (id) => {
+    const product = this.products.find((product) => product.id === id)
+
+    if (!product) {
+      // throw new Error('Not found')
+      console.error('Not found')
+    }
+
+    return product
+  }
+
+  addProduct = ({ title, description, price, thumbnail, code, stock }) => {
+    // Validate required fields
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      // throw new Error('Missing data')
+      console.error('Missing data')
+      return
+    }
+
+    // Validate unique code field
+    const productWithSameCode = this.products.find((product) => product.code === code)
+
+    if (productWithSameCode) {
+      // throw new Error('Code already exists')
+      console.error('Code already exists')
+      return
+    }
+
+    // Create product
+    const id = this.products.length === 0 ? 1 : this.products[this.products.length - 1].id + 1
+
+    const product = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      id,
+    }
+
+    this.products.push(product)
+  }
+}
+
+const pm = new ProductManager()
+
+console.log(pm.getProducts())
+
+pm.addProduct({
+  title: 'producto prueba',
+  description: 'Este es un producto de prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25,
+})
+
+console.log(pm.getProducts())
+
+pm.addProduct({
+  title: 'producto prueba',
+  description: 'Este es un producto de prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25,
+})
+
+console.log(pm.getProductsById(2))
