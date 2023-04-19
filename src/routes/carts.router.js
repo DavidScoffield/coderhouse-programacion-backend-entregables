962f3f9cfa5f0e9a9616de63cb0486695e95f1b8
David Scoffield <davidscoffield01@gmail.com>
2023-04-19 12:58:51 -0300

    Add post /api/carts endpoint

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,9 +1,16 @@
 import { Router } from 'express'
+import CartManager from '../managers/CartManager.js'
 
 const cartRouter = Router()
+const cm = new CartManager()
 
-cartRouter.get('/', (req, res) => {
-  res.json({ message: 'Hello from cart' })
+cartRouter.post('/', async (req, res) => {
+  const cart = await cm.addCart()
+  res.status(201).json({ message: `Cart with id "${cart.id}" created`, cart })
 })
+
+cartRouter.get('/:cid', (req, res) => {})
+
+cartRouter.post('/:cid/product/:pid', (req, res) => {})
 
 export default cartRouter
