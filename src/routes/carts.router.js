6a61ba0bc22471a0704c2f571cc7f42b61b4a42a
David Scoffield <davidscoffield01@gmail.com>
2023-04-19 13:50:14 -0300

    Add endpoint /api/carts/:cid

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,16 +1,29 @@
 import { Router } from 'express'
 import CartManager from '../managers/CartManager.js'
+import { PATH_OF_CARTS } from '../constants/constants.js'
 
 const cartRouter = Router()
-const cm = new CartManager()
+const cm = new CartManager(PATH_OF_CARTS)
 
 cartRouter.post('/', async (req, res) => {
   const cart = await cm.addCart()
-  res.status(201).json({ message: `Cart with id "${cart.id}" created`, cart })
+  res.status(201).json({ message: `Cart with id "${cart.id}" created` })
 })
 
-cartRouter.get('/:cid', (req, res) => {})
+cartRouter.get('/:cid', async (req, res) => {
+  const { cid } = req.params
+  const id = Number(cid)
 
-cartRouter.post('/:cid/product/:pid', (req, res) => {})
+  if (!id || id <= 0) return res.status(400).send({ error: `Invalid id: ${cid}` })
+
+  try {
+    const cart = await cm.getCartById(id)
+    res.send(cart)
+  } catch (error) {
+    res.status(400).send({ error: error.message })
+  }
+})
+
+cartRouter.post('/:cid/product/:pid', async (req, res) => {})
 
 export default cartRouter
