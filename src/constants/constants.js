12582d03d7c219d32027a0af947d17df26616d4a
David Scoffield <davidscoffield01@gmail.com>
2023-04-19 11:46:00 -0300

    Add PATH_OF CARTS

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,6 @@
 // Server
 const PORT = 8080
-const PATH_TO_SAVE_DATA = './products.json'
+const PATH_OF_PRODUCTS = './productos.json'
+const PATH_OF_CARTS = './carrito.json'
 
-export { PORT, PATH_TO_SAVE_DATA }
+export { PORT, PATH_OF_PRODUCTS, PATH_OF_CARTS }
