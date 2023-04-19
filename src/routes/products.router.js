23ffa5fd3dea12ddeeedc978b3f0e2d8db464cac
David Scoffield <davidscoffield01@gmail.com>
2023-04-19 11:46:26 -0300

    Fix import and change message of send to english

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { PATH_TO_SAVE_DATA } from '../constants/constants.js'
+import { PATH_OF_PRODUCTS } from '../constants/constants.js'
 import ProductManager from '../managers/ProductManager.js'
 
 const productRouter = Router()
 
-const pm = new ProductManager(PATH_TO_SAVE_DATA)
+const pm = new ProductManager(PATH_OF_PRODUCTS)
 
 productRouter.get('/', async (req, res) => {
   const { limit } = req.query
@@ -16,7 +16,7 @@ productRouter.get('/', async (req, res) => {
   const limitNumber = Number(limit)
 
   if (!limitNumber || limitNumber <= 0)
-    return res.status(404).send(`"${limit}" no es un numero valido`)
+    return res.status(404).send(`"${limit}" is not a valid limit number`)
 
   res.send(products.slice(0, limitNumber))
 })
