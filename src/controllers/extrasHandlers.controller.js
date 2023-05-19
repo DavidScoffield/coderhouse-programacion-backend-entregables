670577d33ecc88c124b962f125198cc81852e3b6
David Scoffield <davidscoffield01@gmail.com>
2023-05-19 11:44:57 -0300

    Add error handlers

--- src/controllers/extrasHandlers.controller.js
+++ src/controllers/extrasHandlers.controller.js
@@ -1,9 +1,18 @@
+const MONGO_SERVER_ERROR_HANDLER = {
+  DuplicateKey: (res, error) =>
+    res.status(409).json({ error: `Duplicate key error: ${JSON.stringify(error.keyValue)}` }),
+  defaultError: (res) => res.status(400).json({ error: error.menssage }),
+}
+
 const ERROR_HANDLERS = {
   CastError: (res, error) => res.status(400).json({ error: 'Malformatted ID' }),
   ValidationError: (res, error) => res.status(409).json({ error: error.message }),
-  // TokenExpiredError: (res, error) => res.status(400).json({ error: error.message }),
-  // JsonWebTokenError: (res, error) => res.status(400).json({ error: `JWT error: ${error.message}` }),
   MongoError: (res, error) => res.status(400).json({ error: `Mongo error: ${error.message}` }),
+  MongoServerError: (res, error) =>
+    MONGO_SERVER_ERROR_HANDLER[error.codeName](res, error) ||
+    MONGO_SERVER_ERROR_HANDLER.defaultError(res, error),
+  CustomError: (res, error) => res.status(error.status).json({ error: error.message }),
+  ValidationError: (res, error) => res.status(error.status).json({ error: error.message }),
   defaultError: (res) => res.status(500).end(),
 }
 
