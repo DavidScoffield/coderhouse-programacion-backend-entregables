9c908f6becfa64ce1b8efe593178d6da5e0218fb
David Scoffield <davidscoffield01@gmail.com>
2023-05-25 16:48:19 -0300

    fix import

--- src/constants/singletons.js
+++ src/constants/singletons.js
@@ -1,6 +1,6 @@
 import ProductManager from '../dao/mongo/managers/ProductManager.js'
 import CartManager from '../dao/mongo/managers/CartManager.js'
-import MessageManager from '../dao/mongo/Managers/message.manager.js'
+import MessageManager from '../dao/mongo/managers/message.manager.js'
 
 const PM = new ProductManager()
 const CM = new CartManager()
