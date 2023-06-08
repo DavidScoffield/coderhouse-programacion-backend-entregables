fd9b000debed240d2e506fd86686e72acca9ccfa
David Scoffield <davidscoffield01@gmail.com>
2023-06-08 12:22:23 -0300

    Use default admin data object to set session

--- src/middlewares/checkAdminLogin.middleware.js
+++ src/middlewares/checkAdminLogin.middleware.js
@@ -1,22 +1,17 @@
+import { DEFAULT_ADMIN_DATA } from '../constants/constants.js'
+import { ADMIN_PASS, ADMIN_USER } from '../constants/envVars.js'
 import { httpStatus } from '../utils/response.utils.js'
-
-const { ADMIN_USER, ADMIN_PASS } = process.env
 
 export default function checkAdminLogin(req, res, next) {
   const { email, password } = req.body
 
   if (email === ADMIN_USER && password === ADMIN_PASS) {
-    req.session.user = {
-      id: 0,
-      name: 'Admin',
-      role: 'admin',
-      email: '...',
-    }
+    req.session.user = DEFAULT_ADMIN_DATA
 
     return res.send({
       status: httpStatus.SUCCESS,
       message: 'Usuario logeado correctamente',
-      payload: { role: 'admin' },
+      payload: { role: DEFAULT_ADMIN_DATA.role },
     })
   }
 
