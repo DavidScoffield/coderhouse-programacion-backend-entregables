770f921d3951730d1c9e912410dc1051f99767b9
David Scoffield <davidscoffield01@gmail.com>
2023-06-08 12:20:56 -0300

    Add mapped env vars in one file

---
+++ src/constants/envVars.js
@@ -0,0 +1,17 @@
+// # Mongo credentials
+export const MONGO_USER = process.env.MONGO_USER
+export const MONGO_PASS = process.env.MONGO_PASS
+export const MONGO_HOST = process.env.MONGO_HOST
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME
+
+// # App credentials
+export const PERSISTENCE_TYPE = process.env.PERSISTENCE_TYPE
+export const SECRET_SESSION = process.env.SECRET_SESSION || 'secret'
+
+// # Admin credentials
+export const ADMIN_USER = process.env.ADMIN_USER
+export const ADMIN_PASS = process.env.ADMIN_PASS
+
+// # Server credentials
+export const PORT = process.env.SERVER_PORT || 8080
+export const HOST = process.env.SERVER_HOST || 'localhost'
