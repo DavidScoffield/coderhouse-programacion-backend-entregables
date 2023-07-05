e886965ef9121681cee912cc8398a81877e90d12
David Scoffield <davidscoffield01@gmail.com>
2023-07-05 11:23:19 -0300

    Update names of manager files

--- src/constants/singletons.js
+++ src/constants/singletons.js
@@ -1,5 +1,5 @@
-import ProductManager from '../dao/mongo/managers/ProductManager.js'
-import CartManager from '../dao/mongo/managers/CartManager.js'
+import ProductManager from '../dao/mongo/managers/product.manager.js'
+import CartManager from '../dao/mongo/managers/cart.manager.js'
 import MessageManager from '../dao/mongo/managers/message.manager.js'
 import UserManager from '../dao/mongo/managers/user.manager.js'
 
