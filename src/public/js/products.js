d4399fb09e79081e291b9b0f4b89f3ea53e6a7a4
David Scoffield <davidscoffield01@gmail.com>
2023-07-11 11:35:47 -0300

    Update products page to work with the cart id insede of the user jwt

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,26 +1,43 @@
 /* global Swal */
 
-// Recuperar CID (actualmente hardcodeado)
-const cid = '64679c70db83fc11a4f2df62'
+const userData = JSON.parse(document.querySelector('script[data]').getAttribute('data'))
+const { cart: cid } = userData
 
 const tableBody = document.getElementById('table-body')
 
 tableBody.addEventListener('click', async (e) => {
   if (e.target.className === 'btn-add-to-cart') {
     const pid = e.target.getAttribute('product-id')
-    const response = await fetch(`/api/carts/${cid}/product/${pid}`, {
-      method: 'POST',
-    })
-    const data = await response.json()
-    console.log(data)
 
-    Swal.fire({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 2000,
-      title: `Se agrego el producto al carrito`,
-      icon: 'success',
-    })
+    try {
+      const response = await fetch(`/api/carts/${cid}/products/${pid}`, {
+        method: 'POST',
+      })
+
+      const { error, status } = await response.json()
+
+      if (!response.ok || status === 'error') {
+        throw new Error(error)
+      }
+
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000,
+        title: `Se agrego el producto al carrito`,
+        icon: 'success',
+      })
+    } catch (error) {
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000,
+        title: `Error`,
+        text: error.message,
+        icon: 'error',
+      })
+    }
   }
 })
