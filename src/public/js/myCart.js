9f08d264f710d69d90b67d283309e4f4e613eb32
David Scoffield <davidscoffield01@gmail.com>
2023-07-11 00:16:41 -0300

    Add functionality for purchase button

--- src/public/js/myCart.js
+++ src/public/js/myCart.js
@@ -1,5 +1,8 @@
+/* global Swal */
+
 const cartProducts = document.querySelector('.cart-products')
 const cartId = JSON.parse(document.querySelector('script[data]').getAttribute('data'))
+const buyButton = document.querySelector('.buy-button')
 
 cartProducts.addEventListener('click', async (e) => {
   const clickedElement = e.target
@@ -37,5 +40,53 @@ cartProducts.addEventListener('click', async (e) => {
     })
 
     if (response.status === 200) return window.location.reload()
+  }
+})
+
+buyButton.addEventListener('click', async () => {
+  try {
+    const response = await fetch(`/api/carts/${cartId}/purchase`, {
+      method: 'PUT',
+    })
+
+    if (!response.ok) {
+      const { error } = await response.json()
+      throw new Error(error)
+    }
+
+    const { status, payload, error } = await response.json()
+
+    if (status === 'success') {
+      const { ticket } = payload
+      const date = new Date(ticket.purchaseDateTime)
+      const stringDate = date.toLocaleString()
+
+      Swal.fire({
+        title: 'Ticket de compra',
+        icon: 'success',
+        html: `<p>El ticket de compra se ha generado correctamente</p>
+        <p>Código de ticket: <b>${ticket.code}</b></p>
+        <p>Fecha de compra: <b>${stringDate}</b></p>
+        <p>Email: <b>${ticket.purchaser}</b></p>
+        <p>Importe total: <b>$${ticket.amount}</b></p>`,
+        confirmButtonText: 'Cerrar y volver a la tienda',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.replace('/products')
+        }
+      })
+    } else {
+      throw new Error(error)
+    }
+  } catch (e) {
+    Swal.fire({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      title: 'Error en el proceso de compra',
+      text: `${e.message}`,
+      icon: 'error',
+    })
   }
 })
