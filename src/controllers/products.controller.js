5d329ebf3247a18dedb5ad46f1e21e28db2d6c29
David Scoffield <davidscoffield01@gmail.com>
2023-07-25 18:43:15 -0300

    Remove unnecessary try catch in products controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -18,33 +18,29 @@ const getProducts = async (req, res, next) => {
   if (category) productDataToValidate.category = category
   if (status !== undefined) productDataToValidate.status = mappedStatus[status]
 
-  try {
-    isPaginationParamsValid({ limit, page, sort })
-    isProductDataValid(productDataToValidate)
+  isPaginationParamsValid({ limit, page, sort })
+  isProductDataValid(productDataToValidate)
 
-    const { docs, ...rest } = await productRepository.getProducts({
-      limit,
-      page,
-      sort,
-      query: productDataToValidate,
-    })
+  const { docs, ...rest } = await productRepository.getProducts({
+    limit,
+    page,
+    sort,
+    query: productDataToValidate,
+  })
 
-    const payload = {
-      products: docs,
-      totalPages: rest.totalPages,
-      prevPage: rest.prevPage,
-      nextPage: rest.nextPage,
-      page: rest.page,
-      hasNextPage: rest.hasNextPage,
-      hasPrevPage: rest.hasPrevPage,
-      prevLink: rest.prevLink,
-      nextLink: rest.nextLink,
-    }
-
-    res.sendSuccessWithPayload({ message: 'Products found', payload })
-  } catch (error) {
-    next(error)
+  const payload = {
+    products: docs,
+    totalPages: rest.totalPages,
+    prevPage: rest.prevPage,
+    nextPage: rest.nextPage,
+    page: rest.page,
+    hasNextPage: rest.hasNextPage,
+    hasPrevPage: rest.hasPrevPage,
+    prevLink: rest.prevLink,
+    nextLink: rest.nextLink,
   }
+
+  res.sendSuccessWithPayload({ message: 'Products found', payload })
 }
 
 const getProductById = async (req, res, next) => {
@@ -52,16 +48,12 @@ const getProductById = async (req, res, next) => {
 
   if (!pid) return res.status(400).send({ error: `Must to especify an id` })
 
-  try {
-    const id = castToMongoId(pid)
+  const id = castToMongoId(pid)
 
-    const product = await productRepository.getProductById(id)
-    if (!product) return res.sendNotFound({ error: `Product with id "${id}" not found` })
+  const product = await productRepository.getProductById(id)
+  if (!product) return res.sendNotFound({ error: `Product with id "${id}" not found` })
 
-    res.sendSuccessWithPayload({ payload: product })
-  } catch (error) {
-    next(error)
-  }
+  res.sendSuccessWithPayload({ payload: product })
 }
 
 const createProduct = async (req, res, next) => {
@@ -106,65 +98,57 @@ const updateProduct = async (req, res, next) => {
   const { body } = req
   const { title, description, code, price, status, stock, category, thumbnail } = body
 
-  try {
-    const id = castToMongoId(pid)
+  const id = castToMongoId(pid)
 
-    if (
-      !title &&
-      !description &&
-      !price &&
-      !thumbnail &&
-      !code &&
-      !stock &&
-      !category &&
-      status === undefined
-    ) {
-      ErrorService.createValidationError({
-        message: 'Must provide at least one field to update',
-        status: httpStatus.BAD_REQUEST,
-        cause: productErrorAtLeastOne(body),
-        code: EErrors.INCOMPLETE_VALUES,
-      })
-    }
-
-    isProductDataValid(body)
-
-    const updatedProduct = await productRepository.updateProduct(id, {
-      title,
-      description,
-      code,
-      price,
-      status,
-      stock,
-      category,
-      thumbnail,
+  if (
+    !title &&
+    !description &&
+    !price &&
+    !thumbnail &&
+    !code &&
+    !stock &&
+    !category &&
+    status === undefined
+  ) {
+    ErrorService.createValidationError({
+      message: 'Must provide at least one field to update',
+      status: httpStatus.BAD_REQUEST,
+      cause: productErrorAtLeastOne(body),
+      code: EErrors.INCOMPLETE_VALUES,
     })
-
-    res.sendSuccessWithPayload({
-      message: `Product "${updatedProduct._id}" was successfully updated`,
-      payload: updatedProduct,
-    })
-  } catch (error) {
-    next(error)
   }
+
+  isProductDataValid(body)
+
+  const updatedProduct = await productRepository.updateProduct(id, {
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnail,
+  })
+
+  res.sendSuccessWithPayload({
+    message: `Product "${updatedProduct._id}" was successfully updated`,
+    payload: updatedProduct,
+  })
 }
 
 const deleteProduct = async (req, res, next) => {
   const { pid } = req.params
 
-  try {
-    const id = castToMongoId(pid)
+  const id = castToMongoId(pid)
 
-    const deletedProduct = await productRepository.deleteProduct(id)
+  const deletedProduct = await productRepository.deleteProduct(id)
 
-    if (deletedProduct) {
-      res.sendSuccess(`Product "${deletedProduct.id}" was successfully deleted`)
-      req.io.emit('realTimeProducts:storedProducts', await productRepository.getProducts())
-    } else {
-      res.sendNotFound({ error: `Product with id "${id}" not found` })
-    }
-  } catch (error) {
-    next(error)
+  if (deletedProduct) {
+    res.sendSuccess(`Product "${deletedProduct.id}" was successfully deleted`)
+    req.io.emit('realTimeProducts:storedProducts', await productRepository.getProducts())
+  } else {
+    res.sendNotFound({ error: `Product with id "${id}" not found` })
   }
 }
 
