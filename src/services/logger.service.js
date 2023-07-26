c8aa63ecb9a286d754cfa4af0074c00c05d0c4a1
David Scoffield <davidscoffield01@gmail.com>
2023-07-26 19:47:42 -0300

    Refactor loggerService

--- src/services/logger.service.js
+++ src/services/logger.service.js
@@ -2,32 +2,38 @@ import { logger } from '../config/winston.config.js'
 
 export default class LoggerService {
   static fatal(...params) {
-    const arrayParams = [...params].join('\n')
+    const arrayParams = this.#formatParams(params, '\n')
     logger.fatal(arrayParams)
   }
 
   static error(...params) {
-    const arrayParams = [...params].join('\n')
+    const arrayParams = this.#formatParams(params, '\n')
     logger.error(arrayParams)
   }
 
   static info(...params) {
-    const arrayParams = [...params].join(' ')
+    const arrayParams = this.#formatParams(params, ' ')
     logger.info(arrayParams)
   }
 
   static warn(...params) {
-    const arrayParams = [...params].join(' ')
+    const arrayParams = this.#formatParams(params, ' ')
     logger.warning(arrayParams)
   }
 
   static http(...params) {
-    const arrayParams = [...params].join(' ')
+    const arrayParams = this.#formatParams(params, ' ')
     logger.http(arrayParams)
   }
 
   static debug(...params) {
-    const arrayParams = [...params].join(' ')
+    const arrayParams = this.#formatParams(params, ' ')
     logger.debug(arrayParams)
+  }
+
+  static #formatParams = (params, separator) => {
+    return params
+      .map((param) => (typeof param === 'object' ? JSON.stringify(param, null, 2) : param))
+      .join(separator)
   }
 }
