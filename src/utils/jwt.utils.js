73066c1f94590ca803f0b11cffb4e1cf880368bf
David Scoffield <davidscoffield01@gmail.com>
2023-08-14 21:22:39 -0300

    Add verifyToken function

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -6,7 +6,10 @@ export const cookieExtractor = (req) => {
   return req?.cookies?.[COOKIE_AUTH] || null
 }
 
-export const generateToken = (user) => {
-  const token = jwt.sign(user, SECRET_JWT, { expiresIn: '24h' })
-  return token
+export const generateToken = (user, expiresIn = '1d') => {
+  return jwt.sign(user, SECRET_JWT, { expiresIn })
+}
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, SECRET_JWT)
 }
