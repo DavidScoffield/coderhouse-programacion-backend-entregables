e8d28a49ddd50da85d9391311d9da8fcfe273387
David Scoffield <davidscoffield01@gmail.com>
2023-09-06 15:14:51 -0300

    Add helpers to create files in buffer in memory

--- test/helpers.js
+++ test/helpers.js
@@ -1,13 +1,14 @@
+import crypto from 'crypto'
 import fs from 'fs'
 import mongoose from 'mongoose'
 import path from 'path'
 import '../src/config/env.config.js'
-import crypto from 'crypto'
 
 import { MongoSingleton } from '../src/config/mongodb.config.js'
-import { __root } from '../src/utils/dirname.utils.js'
 import { MULTER_PATH_FOLDER } from '../src/constants/constants.js'
+import { __root } from '../src/utils/dirname.utils.js'
 
+const folderPath = path.join(__root, 'test', 'files')
 export const mongoConection = MongoSingleton.getInstance()
 
 export const dropAllCollections = async (collectionNames) => {
@@ -33,18 +34,15 @@ export const dropCollection = async (...collectionNames) => {
   }
 }
 
-const folderPath = path.join(__root, 'test', 'files')
-
-export const generateRandomFiles = ({
+// Function to generate random files in a folder (folderPath).
+export const createRandomFilesInFolder = async ({
   numberOfFiles = 4,
   maxSizeInMB = 10,
   specificSizeInMB = null,
 }) => {
   const filePaths = []
 
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath, { recursive: true })
-  }
+  await fs.promises.mkdir(folderPath, { recursive: true })
 
   for (let i = 0; i < numberOfFiles; i++) {
     // Generate a random file size between 1 MB and maxSizeInMB MB
@@ -62,7 +60,7 @@ export const generateRandomFiles = ({
     const fileContent = crypto.randomBytes(sizeInBytes)
 
     // Write the data to the file
-    fs.writeFileSync(filePath, fileContent)
+    await fs.promises.writeFile(filePath, fileContent)
 
     // Add the file path to the array
     filePaths.push(filePath)
@@ -71,7 +69,57 @@ export const generateRandomFiles = ({
   return filePaths
 }
 
-export const deleteRandomFiles = () => {
-  fs.rmSync(folderPath, { recursive: true })
-  fs.rmSync(MULTER_PATH_FOLDER, { recursive: true })
+// Function to generate random data for a file.
+// Can be used libraries like `crypto` or `faker` to generate random content.
+const generateRandomData = (sizeInMB) => {
+  // create a buffer with random bytes.
+  const bufferSize = sizeInMB * 1024 * 1024 // Convert MB to bytes
+  const randomData = Buffer.alloc(bufferSize)
+  for (let i = 0; i < bufferSize; i++) {
+    randomData[i] = Math.floor(Math.random() * 256) // Random byte (0-255)
+  }
+  return randomData
+}
+
+// Function to create a single random file in memory.
+const createRandomFileInMemory = (sizeInMB) => {
+  const randomData = generateRandomData(sizeInMB)
+  const filename = `${Date.now()}_random_file.txt` // Unique filename based on timestamp
+  return {
+    name: filename,
+    content: randomData,
+  }
+}
+
+// Main function to create random files in memory and return them.
+export const createRandomFilesInMemory = ({
+  numberOfFiles = 4,
+  maxSizeInMB = 10,
+  specificSizeInMB = null,
+}) => {
+  const randomFiles = []
+  for (let i = 0; i < numberOfFiles; i++) {
+    const sizeInMB = specificSizeInMB || Math.random() * (maxSizeInMB - 1) + 1 // Random size between 1MB and maxSizeInMB
+
+    const randomFile = createRandomFileInMemory(sizeInMB)
+    randomFiles.push(randomFile)
+  }
+  return randomFiles
+}
+
+export const deleteRandomFiles = async (...paths) => {
+  const options = {
+    recursive: true,
+    force: true,
+  }
+  try {
+    if (paths.length === 0) {
+      await fs.promises.rm(MULTER_PATH_FOLDER, options)
+      await fs.promises.rm(folderPath, options)
+      return
+    }
+    await Promise.all(paths.map((path) => fs.promises.rm(path, options)))
+  } catch (error) {
+    console.error('Error clearing files:', error)
+  }
 }
