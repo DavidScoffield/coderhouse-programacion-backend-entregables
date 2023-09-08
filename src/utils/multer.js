a5b959210d6778bc1f77a5b9d0ef6484e11c1cd2
David Scoffield <davidscoffield01@gmail.com>
2023-09-08 15:39:29 -0300

    add funciton to extract the name of a document (remove date and extension)

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,4 +1,5 @@
 import multer from 'multer'
+import path from 'path'
 import { MULTER_PATH_FOLDER } from '../constants/constants.js'
 import { MULTER_MAX_FILE_SIZE_MB } from '../constants/envVars.js'
 import FileSystemPromises from '../dao/fileSystem/utils/FileSystemPromises.js'
@@ -24,7 +25,26 @@ export const uploader = multer({
   },
 })
 
+/**
+ * Extracts the relative path of a file.
+ * @param {string} filePath - The path of the file.
+ * @param {string} folder - The folder to extract the relative path from.
+ * @returns {string} The relative path of the file.
+ */
 export const extractToRelativePath = (filePath, folder) => {
   const path = filePath.split(folder)[1]
   return `/${folder}${path}`
+}
+
+/**
+ * Extracts the original name from a filename that has a date prefix, and the extension.
+ * @param {string} filename - The filename to extract the original name from.
+ * @returns {string} The original name of the file.
+ */
+export const extractOriginalName = (filename) => {
+  const nameArray = filename.split('-')
+  // eslint-disable-next-line no-unused-vars
+  const [date, ...rest] = nameArray
+  const withoutDate = rest.join('-')
+  return path.parse(withoutDate).name
 }
